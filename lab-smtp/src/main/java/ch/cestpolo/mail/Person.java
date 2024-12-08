d734e4130b7b6d1eab01a33986f5c35f32b816de
CestPolo <kstar3444@gmail.com>
2024-12-08 19:03:00 +0100

    Person beans implemented

--- lab-smtp/src/main/java/ch/cestpolo/mail/Person.java
+++ lab-smtp/src/main/java/ch/cestpolo/mail/Person.java
@@ -1,12 +1,23 @@
-package ch.cestpolo;
+package ch.cestpolo.mail;
 
+/**
+ * Person class
+ */
 public class Person {
     private String mail;
 
+    /**
+     * Constructor
+     * @param mail
+     */
     public Person(String mail) {
         this.mail = mail;
     }
 
+    /**
+     * Get the person's mail
+     * @return mail
+     */
     public String getMail() {
         return mail;
     }
